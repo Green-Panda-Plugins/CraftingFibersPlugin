5121e3fccd6c766be2ef9f63590e507e3adf766e
SomeGreenDude <70672419+SomeGreenDude@users.noreply.github.com>
2022-06-03 20:51:11 -0700

    Start

---
+++ CraftingFibers/src/main/java/green/misc/CFP.java
@@ -0,0 +1,20 @@
+package green.misc;
+
+import green.misc.Items.ItemManeger;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CFP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[CraftingFibers]: plugin is enabled");
+        ItemManeger.init();
+    }
+
+    @Override
+    public void onDisable() {
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[CraftingFibers]: plugin is disabled");
+    }
+}
---
+++ CraftingFibers/src/main/java/green/misc/Items/ItemManeger.java
@@ -0,0 +1,18 @@
+package green.misc.Items;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.*;
+
+public class ItemManeger {
+    public static void init() { createStringRecipe(); }
+
+    public static void createStringRecipe(){
+        ItemStack item = new ItemStack(Material.STRING);
+        ShapelessRecipe sr = new ShapelessRecipe(NamespacedKey.minecraft("string"), item);
+        sr.addIngredient(1, Material.GRASS);
+        Bukkit.getServer().addRecipe(sr);
+
+    }
+}
